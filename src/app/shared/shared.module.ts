3200811c6f2a46ce90b0070aebf3696e743fd1e6
unknown <plethoraBoy>
2023-07-12 17:17:51 +0530

    Add(shared):add the components in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
+import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
 import { CardComponent } from './components/card/card.component';
 import { SegmentComponent } from './components/segment/segment.component';
 import { ToastComponent } from './components/toast/toast.component';
+import { IonFabComponent } from './components/ion-fab/ion-fab.component';
+
+//SubCompoments
+import { SearchComponent } from './sub-components/search/search.component';
+import { DarkModeToggleComponent } from './sub-components/dark-mode-toggle/dark-mode-toggle.component';
+import { ApexChartsComponent } from './sub-components/apex-charts/apex-charts.component';
 @NgModule({
-  declarations: [InputComponent, CardComponent, SegmentComponent,ToastComponent],
-  imports: [CommonModule, IonicModule],
-  exports: [InputComponent, CardComponent, SegmentComponent,ToastComponent],
+  declarations: [InputComponent, CardComponent, SegmentComponent, ToastComponent, ButtonComponent,IonFabComponent,
+    SearchComponent,DarkModeToggleComponent,ApexChartsComponent],
+  imports: [CommonModule, IonicModule,FormsModule],
+  exports: [InputComponent, CardComponent, SegmentComponent, ToastComponent, ButtonComponent,IonFabComponent,
+    SearchComponent,DarkModeToggleComponent,ApexChartsComponent
+  ],
 })
 export class SharedModule { }
