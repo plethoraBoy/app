6430e0efa6e9eca8978d4b25aef9dc03d3d4c419
unknown <plethoraBoy>
2023-07-12 17:20:58 +0530

    Change(layout):add the shared modulein layout module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [HeaderComponent,SidebarComponent,FooterComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule,SharedModule],
   exports: [HeaderComponent,SidebarComponent,FooterComponent],
 })
 export class LayoutModule {}
