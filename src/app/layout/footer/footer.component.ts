23223729e9bd5017cf26db42f5e65ec8e2b3416a
unknown <plethoraBoy>
2023-07-12 17:13:12 +0530

    Add(Footer):add the ion-fab in the footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent  implements OnInit {
+export class FooterComponent implements OnInit {
+  isAndroid!: boolean;
+  isIos!:boolean
+  isDesktop!: boolean;
 
-  constructor() { }
+constructor(public  platform : Platform) { }
 
-  ngOnInit() {}
+ngOnInit() { 
+
+  // this.isAndroid = this.platform.is('android');
+  // this.isIos = this.platform.is('ios');
+  this.isDesktop = this.platform.is('desktop');
+
+}
+
 
 }
